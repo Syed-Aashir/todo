415f834280b52bfaff9815a424b301feabe63940
laiba166-shaikh <laibas166@gmail.com>
2023-01-07 23:46:18 +0500

    add contact and show data

--- src/components/home.js
+++ src/components/home.js
@@ -1,71 +1,110 @@
 import React from 'react';
-import {useState} from 'react'
-import { BrowserRouter, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useNavigate,
+} from 'react-router-dom';
 
-function Home(){
-    const [search, setSearch] = useState("");
-    function getData(e){
-        setSearch(e.target.value);
-    }
-    const[order, setOrder] = useState("");
-    function getOrderData(e){
-        setOrder(e.target.value);
-    }
-    const[filter, setFilter] = useState("");
-    function getFilterData(e){
-        setFilter(e.target.value);
-    }
-    const navigate = useNavigate();
-    function logValues(){
-        console.log(search);
-        console.log(order);
-        console.log(filter);
-        navigate('/newContact');
-    }
-    return(
-        <div>
-            <div class="container flex">
-        <div class="box-wrapper">
-            <div class="filters-wrapper">
-                <h2>Contacts</h2>
-                <label><strong>Search Contact:</strong></label>
-                <input
-                type="text"
-                name="searchContact"
-                id="search-contact"
-                placeholder="Search by name"
-                onChange={getData}
-                />
-                <div class="filter-container">
-                    <div class="order-wrapper">
-                    <label><strong>Order by:</strong></label>
-                    <select id="order-select" onClick={getOrderData} defaultValue = {""}>
-                        <option value="" disabled hidden>All Enteries</option>
-                        <option value="1">Alphabetical order</option>
-                        <option value="-1">Reverse Order</option>
-                    </select>
-                    </div>
-                    <div class="filter-wrapper">
-                    <label><strong>Filter by:</strong></label>
-                    <select id="filter-select" onClick={getFilterData}>
-                        <option value="">All Contacts</option>
-                        <option value="phoneNumber">Contacts with phone Numbers</option>
-                        <option value="emailAddress">Contacts having Email Id</option>
-                    </select>
-                    </div>
-                </div>
-            </div>
+function Home() {
+  const navigate = useNavigate();
 
-            <div id="contacts-list">
-            </div>
+  const [contacts, setContacts] = useState([]);
+  const [search, setSearch] = useState('');
+  function getData(e) {
+    setSearch(e.target.value);
+  }
 
-            <div id="pagination-wrapper">
+  const [order, setOrder] = useState('');
+  function getOrderData(e) {
+    setOrder(e.target.value);
+  }
+
+  const [filter, setFilter] = useState('');
+  function getFilterData(e) {
+    setFilter(e.target.value);
+  }
+
+  function logValues() {
+    console.log(search);
+    console.log(order);
+    console.log(filter);
+    navigate('/newContact');
+  }
+
+  useEffect(() => {
+    // mounting -> dependency array is empty
+    const contacts = JSON.parse(localStorage.getItem('contactsList'));
+    if (contacts) {
+      setContacts([...contacts]);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div class='container flex'>
+        <div class='box-wrapper'>
+          <div class='filters-wrapper'>
+            <h2>Contacts</h2>
+            <label>
+              <strong>Search Contact:</strong>
+            </label>
+            <input
+              type='text'
+              name='searchContact'
+              id='search-contact'
+              placeholder='Search by name'
+              onChange={getData}
+            />
+            <div class='filter-container'>
+              <div class='order-wrapper'>
+                <label>
+                  <strong>Order by:</strong>
+                </label>
+                <select
+                  id='order-select'
+                  onClick={getOrderData}
+                  defaultValue={''}
+                >
+                  <option value='' disabled hidden>
+                    All Enteries
+                  </option>
+                  <option value='1'>Alphabetical order</option>
+                  <option value='-1'>Reverse Order</option>
+                </select>
+              </div>
+              <div class='filter-wrapper'>
+                <label>
+                  <strong>Filter by:</strong>
+                </label>
+                <select id='filter-select' onClick={getFilterData}>
+                  <option value=''>All Contacts</option>
+                  <option value='phoneNumber'>
+                    Contacts with phone Numbers
+                  </option>
+                  <option value='emailAddress'>Contacts having Email Id</option>
+                </select>
+              </div>
             </div>
-            <a id="new-contact-button" onClick={logValues}>Add Contact</a>
+          </div>
+
+          <div id='contacts-list'>
+            {contacts?.map((contact) => (
+              <div> {contact.firstName}</div>
+            ))}
+          </div>
+
+          <div id='pagination-wrapper'></div>
+          <a id='new-contact-button' onClick={logValues}>
+            Add Contact
+          </a>
         </div>
+      </div>
     </div>
-        </div>
-    );
+  );
 }
 
 export default Home;
--- src/components/newContact.js
+++ src/components/newContact.js
@@ -1,77 +1,85 @@
-import {useState} from 'react'
-import { func } from 'prop-types';
+import { useState } from 'react';
 
-function NewContact(){
-    const [fname, setFname] = useState("");
-    function firstName(e){
-        setFname(e.target.value);
-    }
+function NewContact() {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  });
 
-    const [lname, setLname] = useState("");
-    function lastName(e){
-        setLname(e.target.value);
-    }
+  function onChangeInput(e) {
+    const value = e.target.value;
+    const inputName = e.target.name;
+    setUser({
+      ...user,
+      // firstName:value
+      [inputName]: value,
+    });
+  }
 
-    const [phone, setPhone] = useState("");
-    function phoneNumber(e){
-        setPhone(e.target.value);
+  function getContactValues(e) {
+    e.preventDefault();
+    let contactsList = JSON.parse(localStorage.getItem('contactsList'));
+    if (contactsList) {
+      contactsList = [...contactsList, { ...user }];
+    } else {
+      contactsList = [{ ...user }];
     }
+    console.log('contact list ->', contactsList);
+    localStorage.setItem('contactsList', JSON.stringify(contactsList));
+  }
 
-    const [email, setEmail] = useState("");
-    function emailAddress(e){
-        setEmail(e.target.value);
-    }
-
-    function getContactValues(){
-        console.log(fname)
-        console.log(lname)
-        console.log(phone)
-        console.log(email)
-    }
-    return(
-        <div>
-            <div className="container flex">
-      <div className="box-wrapper">
-        <div className="add-form">
-          <h2>Add Contact</h2>
-          <form action="" id="add-contact-form">
-            <input
-              type="text"
-              name="firstName"
-              id="fname"
-              placeholder="First Name"
-              onChange={firstName}
-            />
-            <input
-              type="text"
-              name="lastName"
-              id="lname"
-              placeholder="Last Name"
-              onChange={lastName}
-            />
-            <input
-              type="tel"
-              name="phoneNumber"
-              id="phone"
-              placeholder="Phone Number (optional)"
-              onChange={phoneNumber}
-            />
-            <div id="phone-no-err-msg"></div>
-            <input
-              type="email"
-              name="emailAddress"
-              id="email"
-              placeholder="Email (optional)"
-              onChange={emailAddress}
-            />
-            <br />
-            <button className="form-btn" onClick={getContactValues}>Add Contact</button>
-          </form>
+  return (
+    <div>
+      <div className='container flex'>
+        <div className='box-wrapper'>
+          <div className='add-form'>
+            <h2>Add Contact</h2>
+            <form action='' onSubmit={getContactValues} id='add-contact-form'>
+              <input
+                type='text'
+                name='firstName'
+                id='fname'
+                value={user.firstName}
+                placeholder='First Name'
+                onChange={onChangeInput}
+              />
+              <input
+                type='text'
+                name='lastName'
+                id='lname'
+                value={user.lastName}
+                placeholder='Last Name'
+                onChange={onChangeInput}
+              />
+              <input
+                type='tel'
+                name='phoneNumber'
+                id='phone'
+                value={user.phoneNumber}
+                placeholder='Phone Number (optional)'
+                onChange={onChangeInput}
+              />
+              <div id='phone-no-err-msg'></div>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                value={user.email}
+                placeholder='Email (optional)'
+                onChange={onChangeInput}
+              />
+              <br />
+              <button className='form-btn' type='submit'>
+                Add Contact
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-        </div>
-    )
+  );
 }
 
 export default NewContact;
